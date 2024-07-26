9f5add49cb4befa7f346d36e3866077178034904
Matheus da Silva de Paula <matheusdepaula@wisecut.video>
2024-07-26 10:54:01 -0300

    feat; cadd carts module to app

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ProductsModule } from './products/products.module';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { CartModule } from './cart/cart.module';
 
 @Module({
-  imports: [ProductsModule, PrismaModule],
+  imports: [ProductsModule, PrismaModule, CartModule],
   controllers: [],
   providers: [],
 })
