a039fe18773a26e760c8a810915867e2afb56e01
Matheus da Silva de Paula <matheusdepaula@wisecut.video>
2024-07-26 09:53:39 -0300

    feat: create route for create product

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,10 +1,14 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import { InputCreateProductDto } from './dto/product.dto';
+import { ProductsService } from './products.service';
 
 @Controller('products')
 export class ProductsController {
+    constructor(private readonly productsService: ProductsService) { }
     @Post()
-    createProduct() {
-        
-        
+    @HttpCode(201)
+    async createProduct(@Body() input: InputCreateProductDto) {
+        await this.productsService.createProduct(input);
+        return { message: 'Product created successfully' };
     }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { InputCreateProductDto } from './dto/product.dto';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+    private readonly logger = new Logger(ProductsService.name);
+    constructor(private prismaService: PrismaService){}
+
+    async createProduct(input: InputCreateProductDto) {
+
+        try {
+            await this.prismaService.product.create({
+                data: {
+                    name: input.name,
+                    price: input.price,
+                    quantity: input.quantity,
+                    description: input.description,
+                    image: input.image
+                }
+            });
+            
+        } catch (error) {
+            this.logger.error('Error fetching user', error);
+            throw new InternalServerErrorException('An error occurred');
+        }
+    }
+}
