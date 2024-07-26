f526810c00683e0b8662e7667143bfe529bdab46
Matheus da Silva de Paula <matheusdepaula@wisecut.video>
2024-07-26 14:18:19 -0300

    test: create tests for method store

--- src/cart/cart.service.spec.ts
+++ src/cart/cart.service.spec.ts
@@ -27,6 +27,7 @@ describe('CartService', () => {
   });
 
   it('should create a new cart', async () => {
+
     jest.spyOn(prisma.cart, 'create')
     .mockResolvedValue({
       id: 1,
@@ -43,7 +44,37 @@ describe('CartService', () => {
     jest.spyOn(prisma.cart, 'create')
     .mockRejectedValue(new Error('Test error'));
     const input = { items: [{ productId: 1, quantity: 1 }] };
-    await expect(service.storeCart(input)).rejects.toThrow('An error occurred');
+    await expect(service.storeCart(input)).rejects.toThrow('Test error');
     expect(prisma.cart.create).toHaveBeenCalled();
   })
+
+  it('should throw an error when trying to create a cart with non-existent product', async () => {
+    jest.spyOn(prisma.product, 'findUnique')
+    .mockResolvedValue(null);
+    jest.spyOn(prisma.cart, 'create')
+    .mockResolvedValue({
+      id: 1,
+      productId: 1,
+      quantity: 1
+    });
+    const input = { items: [{ productId: 1, quantity: 1 }] };
+    await expect(service.storeCart(input)).rejects.toThrow('Product with id 1 not found.');
+    expect(prisma.product.findUnique).toHaveBeenCalled();
+    
+  })
+
+  it('should throw an error when trying to create a cart with insufficient product quantity', async () => {
+    jest.spyOn(prisma.product, 'findUnique')
+    .mockResolvedValue({ id: 1, quantity: 5, description: 'teste', image: 'teste.png', price:55, name: 'teste' });
+    jest.spyOn(prisma.cart, 'create')
+    .mockResolvedValue({
+      id: 1,
+      productId: 1,
+      quantity: 1
+    });
+    const input = { items: [{ productId: 1, quantity: 6 }] };
+    await expect(service.storeCart(input)).rejects.toThrow('Insufficient quantity for product with id 1. Available: 5');
+    expect(prisma.product.findUnique).toHaveBeenCalled();
+  })
+  
 });
